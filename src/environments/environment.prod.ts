23c1dd047d6a29e93ae206b1117f8d0cac3ac879
MatePaic <57683081+MatePaic@users.noreply.github.com>
2022-05-04 16:11:57 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyASaZnHw_8wxH-yTWCLtzdagPosUDincpE",
+    authDomain: "clips-98f7f.firebaseapp.com",
+    projectId: "clips-98f7f",
+    storageBucket: "clips-98f7f.appspot.com",
+    appId: "1:421882319179:web:6b6d8851255d9e028ed8ce"
+  }
+};
